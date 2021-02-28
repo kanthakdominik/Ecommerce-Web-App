1168e643a8905b13a1f4afed3a01ed5360153b05
kanthakdominik <kanthakdominik@gmail.com>
2021-02-28 20:31:00 +0100

    created empty project

---
+++ src/test/java/pl/dominik/ecommercewebapp/EcommerceWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dominik.ecommercewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
