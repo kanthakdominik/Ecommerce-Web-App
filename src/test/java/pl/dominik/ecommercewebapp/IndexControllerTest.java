98cf5b65f8732eb1008fe9da3722f3057d97f373
kanthakdominik <kanthakdominik@gmail.com>
2021-03-14 14:35:23 +0100

    added IndexControllerTest

---
+++ src/test/java/pl/dominik/ecommercewebapp/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package pl.dominik.ecommercewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class IndexControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void returnsLandingPage() throws Exception{
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Cake Factory")));
+    }
+}
