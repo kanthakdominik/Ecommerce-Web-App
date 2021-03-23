ad3ad9c730fd72b1dadb80e4bf8d64d9aed9026f
kanthakdominik <kanthakdominik@gmail.com>
2021-03-24 00:40:24 +0100

    deleted EcommerenceWebAppApplication added shouldViewListOfItemsFromDatabase test to CatalogControllerTest

--- src/test/java/pl/dominik/ecommercewebapp/catalog/CatalogControllerTest.java
+++ src/test/java/pl/dominik/ecommercewebapp/catalog/CatalogControllerTest.java
@@ -1,11 +1,23 @@
 package pl.dominik.ecommercewebapp.catalog;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,12 +29,37 @@ public class CatalogControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    CatalogService catalogService;
+
+    private WebClient webClient;
+
+    @BeforeEach
+    public void setup() {
+        webClient = MockMvcWebClientBuilder.mockMvcSetup(mockMvc).build();
+    }
+
     @Test
-    void returnsLandingPage() throws Exception{
-        mockMvc.perform(get("http://localhost:8080/catalog"))
+    @DisplayName("index page returns the landing page")
+    void shouldReturnLandingPage() throws Exception {
+        mockMvc.perform(get("http://localhost:8080/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello from catalog")));
+                .andExpect(content().string(containsString("Cake Factory")));
+    }
+
+    @Test
+    @DisplayName("index page return a list of items from the database")
+    void shouldViewListOfItemsFromDatabase() throws Exception {
+        String expectedTitle = "Test title";
+        BigDecimal expectedPrice = BigDecimal.ONE;
+        List<Item> list = new ArrayList<>();
+        Item testItem = new Item(expectedTitle, expectedPrice);
+        list.add(testItem);
+        when(catalogService.getAll()).thenReturn(list);
+        HtmlPage page = webClient.getPage("http://localhost:8080/");
+
+        assertThat(page.querySelectorAll(".item-title")).anyMatch(domElement -> domElement.getTextContent().equals(expectedTitle));
     }
 }
 
